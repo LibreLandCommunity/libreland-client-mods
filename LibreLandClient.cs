083b5cddee72884cc127eeffcc9ab0ab1e247a1f
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2025-01-10 10:42:29 +1100

    Initial push

---
+++ LibreLandClient.cs
@@ -0,0 +1,20 @@
+﻿namespace LibreLandClient;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+
+public static class PluginInfo {
+    public const string GUID = "dev.librelandcommunity.client";
+    public const string NAME = "LibreLand-Client";
+    public const string VERSION = "0.0.1";
+
+}
+
+[BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+public class LibreLandClient : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo($"Plugin {PluginInfo.GUID} is loaded!");
+    }
+}
