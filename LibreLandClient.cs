e47f261d507594b5ae45e8d7bd972b52949ead1d
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2025-01-10 11:08:17 +1100

    Start of patching endpoints

--- LibreLandClient.cs
+++ LibreLandClient.cs
@@ -7,7 +7,6 @@ public static class PluginInfo {
     public const string GUID = "dev.librelandcommunity.client";
     public const string NAME = "LibreLand-Client";
     public const string VERSION = "0.0.1";
-
 }
 
 [BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
@@ -16,5 +15,20 @@ public class LibreLandClient : BaseUnityPlugin
     private void Awake()
     {
         Logger.LogInfo($"Plugin {PluginInfo.GUID} is loaded!");
+
+        Harmony harmony = new Harmony(PluginInfo.GUID);
+        harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(ServerManager), nameof(ServerManager.RemoteServerBaseUrl), MethodType.Getter)]
+    private static bool PatchRemoteServerBaseUrl(string __result){
+        __result = "Deez";
+        return false;
+    }
+
+    // Test patch to see if changing stuff worked
+    [HarmonyPatch(typeof(ServerManager), nameof("Setup"))]
+    private static bool ServerManagerPatch() {
+        return false;
     }
 }
